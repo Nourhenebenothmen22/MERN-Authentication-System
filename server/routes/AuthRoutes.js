0942967b2e9bc90b852f3148218765156edb7604
nourhene <benothmennourhen8@gmail.com>
2025-10-21 22:21:58 +0100

    🐛 Fix module import path for authController.js to resolve server crash

--- server/routes/AuthRoutes.js
+++ server/routes/AuthRoutes.js
@@ -1,6 +1,5 @@
-// routes/authRoutes.js
 import express from "express";
-import { register, login, logout } from "../controllers/authController.js";
+import { register, login, logout } from "../controllers/userAuth.js";
 
 const router = express.Router();
 
