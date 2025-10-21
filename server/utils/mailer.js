bca67219074e0afcb7c7d5e9ba6de222ce657164
nourhene <benothmennourhen8@gmail.com>
2025-10-21 23:54:38 +0100

    Configure mailer settings

--- server/utils/mailer.js
+++ server/utils/mailer.js
@@ -1,11 +1,24 @@
-
+// utils/mailer.js
 import nodemailer from "nodemailer";
+
+// Create a Nodemailer transporter instance
 const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,       
-  port: process.env.SMTP_PORT ,  
+  host: 'sandbox.smtp.mailtrap.io',
+  port: 2525,
+  secure: false, // Mailtrap does not use TLS for this sandbox
   auth: {
-    user: process.env.SMTP_USER,     // SMTP username
-    pass: process.env.SMTP_PASS,     // SMTP password
+    user: '42c2640dbbb26a',
+    pass: 'd667870a7333e5',
   },
 });
-export default transporter
+
+// Optional: verify connection
+transporter.verify((error, success) => {
+  if (error) {
+    console.error("❌ SMTP connection error:", error);
+  } else {
+    console.log("✅ SMTP server is ready to send emails");
+  }
+});
+
+export default transporter;
