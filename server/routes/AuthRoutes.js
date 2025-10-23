c9262f1620c43969b2b573c66c5f5b0e26ac02c2
nourhene <benothmennourhen8@gmail.com>
2025-10-23 18:23:09 +0100

    🔧 Update AuthRoutes.js: ajout des nouvelles routes verifyEmail et send-verify-otp

--- server/routes/AuthRoutes.js
+++ server/routes/AuthRoutes.js
@@ -108,7 +108,7 @@ router.post("/login", login);
  *         description: Non authentifié ou token invalide
  */
 router.post("/logout", logout);
-router.post("/send-verify-otp",userAuth,sendVerifyOtp)
+router.post("/send-verify-otp",sendVerifyOtp)
 router.post('/verify-account',userAuth,verifyEmail)
 
 export default router;
