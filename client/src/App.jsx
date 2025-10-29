f903e47e2ec22cfab3f1c520b11a966087ff323b
nourhene <benothmennourhen8@gmail.com>
2025-10-29 21:17:46 +0100

    🛣️ Mise à jour des routes et gestion auth dans App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import ResetPassword from './pages/ResetPassword'
+import EmailVerify from './pages/EmailVerify'
 
 function App() {
   return (
-    <h1 class="text-5xl font-bold underline">
-    Hello world!
-  </h1>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/register' element={<Register/>} />
+      <Route path='/login' element={<Login/>} />
+      <Route path='/reset-password' element={<ResetPassword/>}/>
+      <Route path='/email-verify' element={<EmailVerify/>}/>
+    </Routes>
   )
 }
 
