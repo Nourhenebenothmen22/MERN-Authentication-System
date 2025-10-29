a6fce0c2a1313f327fa7423361f783569d8da4f4
nourhene <benothmennourhen8@gmail.com>
2025-10-29 20:32:53 +0100

    ✨ Mise à jour App.jsx et ajout des pages côté client avec Tailwind

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function App() {
   return (
-    <h1 class="text-3xl font-bold underline">
+    <h1 class="text-5xl font-bold underline">
     Hello world!
   </h1>
   )
